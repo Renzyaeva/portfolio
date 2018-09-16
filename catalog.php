aa92797314f54b24910afbddcc787a2e74c9fc56
Irchik <i.renz@mail.ru>
2018-09-16 23:15:20 +0400

    Создана базовая заготовка для будущего портфолио

---
+++ catalog.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Портфолио</title>
+	</head>
+	<body>
+		<header>
+			<div>
+				Мое портфолио
+			</div>
+			<div>				
+				<a href="index.php">Главная страница</a>
+				<span>Каталог работ</span>
+			</div>
+		</header>
+		<div>
+			Добро пожаловать
+		</div>
+		<h1>Каталог работ</h1>
+		<footer>
+			Здесь будет подвал
+		</footer>
+		
+		<?php echo 'test'; ?>
+
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Портфолио</title>
-</head>
-<body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Портфолио работ">
+		<meta name="keywords" content="Портфолио, работа">
+		<title>Портфолио</title>
+		<link rel="stylesheet" href="styles/main.css">
+		<link rel="icon" href="favicon.png" type="image/png">
+	</head>
+	<body>
+		<header>
+			<div class="headerInside"></div>
+		</header>
+		<div>
+			Добро пожаловать
+		</div>
+		<h1>Мое портфолио</h1>
+		<img src="images/profile2.jpg" alt="Рензяева И.А.">
+		<footer>
+			Здесь будет подвал
+		</footer>
+		
+		<?php echo 'test'; ?>
 
-</body>
+
+	</body>
 </html>
